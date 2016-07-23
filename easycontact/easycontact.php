426d0202e2eea274c1290d64797c1864a23049b7
CrazyFreeMan <yaroslav.romanenko@gmail.com>
2016-07-23 22:16:26 +0300

    маленькі правки

--- easycontact/easycontact.php
+++ easycontact/easycontact.php
@@ -1,4 +1,4 @@
-<?php
+<?php defined('COT_CODE') or die('Wrong URL');
 /* ====================
 [BEGIN_COT_EXT]
 Hooks=global
@@ -9,66 +9,65 @@ Hooks=global
  * easycontact
  *
  * @package easycontact
- * @version 0.2
+ * @version 0.3
  * @author CrazyFreeMan
- * @copyright Copyright (c) CrazyFreeMan 2014
+ * @copyright Copyright (c) CrazyFreeMan 2016
  * @license BSD
- */
-defined('COT_CODE') or die('Wrong URL');	
-		require_once cot_langfile('easycontact', 'plug'); 
-		require_once cot_incfile('forms');	
+ */	
+require_once cot_langfile('easycontact', 'plug'); 
+require_once cot_incfile('forms');	
 	//генерація форми
-	function cot_easycontact_form($redirect = '/') {		
-		global $cfg, $L, $usr;
+function cot_easycontact_form($redirect = '/') {		
+	global $cfg, $L, $usr;
 
-		if(($cfg['plugin']['easycontact']['only_for_registred'] == '1' && $usr['id'] > 0) || 
-			($cfg['plugin']['easycontact']['only_for_registred'] == '0')) {
+	if(($cfg['plugin']['easycontact']['only_for_registred'] == '1' && $usr['id'] > 0) || 
+		($cfg['plugin']['easycontact']['only_for_registred'] == '0')) {
 
 
-			$input_array = array();		
-			$array_config = array( 	
-									"user_name" => array("name" => "use_input_name", "value" => ""),
-									"user_email" => array("name" => "use_input_email", "value" => ""),
-									"user_phone" => array("name" => "use_input_phone", "value" => ""),
-									"mail_subj" => array("name" => "use_input_subj", "value" => "")
-									
-									);
+		$input_array = array();		
+	$array_config = array( 	
+		"user_name" => array("name" => "use_input_name", "value" => ""),
+		"user_email" => array("name" => "use_input_email", "value" => ""),
+		"user_phone" => array("name" => "use_input_phone", "value" => ""),
+		"mail_subj" => array("name" => "use_input_subj", "value" => "")
+		
+		);
 
-			
-		if (isset($_POST['ecsend']))
-		{ 
+	
+	if (isset($_POST['ecsend']))
+	{ 
 			//перевірка імені відправника
-			if($cfg['plugin']['easycontact']['use_input_name'] == '1'){		
-				$user_name = cot_import('user_name', 'P', 'TXT');				
-				if (empty($user_name) || mb_strlen($user_name) < 3)
-				{
-				    cot_error($L["err_name"], 'user_name');
-				}else{
+		if($cfg['plugin']['easycontact']['use_input_name'] == '1'){		
+			$user_name = cot_import('user_name', 'P', 'TXT');				
+			if (empty($user_name) || mb_strlen($user_name) < 3)
+			{
+				cot_error($L["err_name"], 'user_name');
+			}else{
 				$rtn["user_name"] = $user_name;
-				}
-			}else{
-				$rtn["user_name"] = false;
 			}
+		}else{
+			$rtn["user_name"] = false;
+		}
 			//перевірка пошти відправника
-			if($cfg['plugin']['easycontact']['use_input_email'] == '1'){		
-				$user_email = cot_import('user_email', 'P', 'TXT');
-				if (empty($user_email) || !cot_check_email($user_email))
-				{
-				    cot_error($L["err_mail"], 'user_email');
-				}else{
-						$rtn["user_email"] = $user_email;					
-				}
+		if($cfg['plugin']['easycontact']['use_input_email'] == '1'){		
+			$user_email = cot_import('user_email', 'P', 'TXT');
+			if (empty($user_email) || !cot_check_email($user_email))
+			{
+				cot_error($L["err_mail"], 'user_email');
 			}else{
-				$rtn["user_email"] = false;
+				$rtn["user_email"] = $user_email;					
 			}
+		}else{
+			$rtn["user_email"] = false;
+		}
 			//перевірка телефону відправника
-			if($cfg['plugin']['easycontact']['use_input_phone'] == '1'){		
-				$user_phone = cot_import('user_phone', 'P', 'TXT');
+		if($cfg['plugin']['easycontact']['use_input_phone'] == '1'){		
+			$user_phone = cot_import('user_phone', 'P', 'TXT');
 				if (empty($user_phone) || mb_strlen($user_phone) < $cfg['plugin']['easycontact']['input_phone_countnum'] ) //|| !is_numeric($user_phone) перевірка на число не актуально
 				{
-				    cot_error($L["err_phone"], 'user_phone');
+					cot_error($L["err_phone"], 'user_phone');
 				}else{
-				$rtn["user_phone"] = $user_phone;
+					$rtn["user_phone"] = $user_phone;
 				}
 
 			}else{
@@ -79,9 +78,9 @@ defined('COT_CODE') or die('Wrong URL');
 				$mail_subj = cot_import('mail_subj', 'P', 'TXT');
 				if (empty($mail_subj) || mb_strlen($mail_subj) < 3)
 				{
-				    cot_error($L["err_subj"], 'mail_subj');
+					cot_error($L["err_subj"], 'mail_subj');
 				}else{
-				$rtn["mail_subj"] = $mail_subj;
+					$rtn["mail_subj"] = $mail_subj;
 				}
 			}else{
 				$rtn["mail_subj"] = false;
@@ -97,22 +96,22 @@ defined('COT_CODE') or die('Wrong URL');
 				$semail = ($cfg['plugin']['easycontact']['use_system_email'] == "1") ? $cfg['adminemail']: str_replace(' ', '', $cfg['plugin']['easycontact']['email_for_send']);
 				if(cot_check_email($semail)){
 					$semail	= $cfg['adminemail'];
-					}
+				}
 
-					if($rtn["user_name"] == false){$rtn["user_name"] = $L["nodata"];}
-					if($rtn["user_email"] == false){$rtn["user_email"] = $L["nodata"];}
-					if($rtn["mail_subj"] == false){$rtn["mail_subj"] = $L["nodata"];}
-					if($rtn["user_phone"] == false){$rtn["user_phone"] = $L["nodata"];}
+				if($rtn["user_name"] == false){$rtn["user_name"] = $L["nodata"];}
+				if($rtn["user_email"] == false){$rtn["user_email"] = $L["nodata"];}
+				if($rtn["mail_subj"] == false){$rtn["mail_subj"] = $L["nodata"];}
+				if($rtn["user_phone"] == false){$rtn["user_phone"] = $L["nodata"];}
 
-					$headers = ("From: \"" . $rtn["user_name"] . "\" <" . $rtn["user_email"] . ">\n");
+				$headers = ("From: \"" . $rtn["user_name"] . "\" <" . $rtn["user_email"] . ">\n");
 
-					$text = $L["user_name"]." - ".$rtn["user_name"]."\n\r";
-					$text .= $L["mail_subj"]." - ".$rtn["mail_subj"]."\n\r"; 
-					$text .= $L["user_phone"]." - ".$rtn["user_phone"];						
-					
-					cot_mail($semail, $cfg['plugin']['easycontact']['email_subj_add']." - ".$rtn['mail_subj'], $text, $headers);
-					cot_log('Easycontact mail send', 'plg');
-					
+				$text = $L["user_name"]." - ".$rtn["user_name"]."\n\r";
+				$text .= $L["mail_subj"]." - ".$rtn["mail_subj"]."\n\r"; 
+				$text .= $L["user_phone"]." - ".$rtn["user_phone"];						
+				
+				cot_mail($semail, $cfg['plugin']['easycontact']['email_subj_add']." - ".$rtn['mail_subj'], $text, $headers);
+				cot_log('Easycontact mail send', 'plg');
+				
 				$array_config["user_name"]["value"] = "";
 				$array_config["user_email"]["value"]  = "";
 				$array_config["user_phone"]["value"]  = "";
@@ -129,7 +128,7 @@ defined('COT_CODE') or die('Wrong URL');
 				if (($key == "user_name" || $key ==  "user_email") && $usr['id'] > 0) {
 					$custom_add = $necess."' id='".$key."' readonly='readonly'";
 					$input_array[$key]= cot_inputbox('text',$key,$usr["profile"][$key],$custom_add);				
-											}else{							
+				}else{							
 					$custom_add = "placeholder='".$L[$key]." ".$necess."' id='".$key."'";
 					$input_array[$key]= cot_inputbox('text',$key, $value["value"],$custom_add);
 				}
@@ -144,7 +143,7 @@ defined('COT_CODE') or die('Wrong URL');
 		}	
 
 		cot_display_messages($mytpl);
-	
+		
 		//$easy_form_arr['EASYCONTACT_FORM_SEND'] = cot_url('plug', 'e=easycontact');
 		
 		$easy_form_arr['EASYCONTACT_FORM_SEND'] = $redirect;		
@@ -153,5 +152,5 @@ defined('COT_CODE') or die('Wrong URL');
 		$mytpl->parse('MAIN');
 
 		return $mytpl->text('MAIN');
-		}		 
-	}
+	}		 
+}
--- easycontact/easycontact.setup.php
+++ easycontact/easycontact.setup.php
@@ -1,31 +1,34 @@
-<?php
+<?php defined('COT_CODE') or die('Wrong URL');
 /* ====================
 [BEGIN_COT_EXT]
-Name=Easy contact
-Code=easycontact
+Code=Easy contact
+Name=easycontact
 Description=Простая форма обращения пользователя к администрации
-Version=0.2
-Date=2014-jun-16
+Version=0.3
+Date=2016-07-23
 Author=CrazyFreeMan
-Copyright=(c) CrazyFreeMan
-Notes=BSD License
+Copyright=&copy; CrazyFreeMan (www.simple-website.in.ua) 2016
+Notes=BSD
 Auth_guests=R
-Lock_guests=W12345A
-Auth_members=R
-Lock_members=W12345A
+Lock_guests=2345A
+Auth_members=RW
+Lock_members=2345
+Requires_modules=
+Requires_plugins=
+Recommends_modules=
+Recommends_plugins=
 [END_COT_EXT]
-  
+
 [BEGIN_COT_EXT_CONFIG]
-only_for_registred=01:radio:0,1:1
-use_system_email=02:radio:0,1:1
-email_for_send=03:string
-use_input_name=04:radio:0,1:1
-use_input_email=05:radio:0,1:1
-use_input_phone=06:radio:0,1:1
+only_for_registred=01:radio::1
+use_system_email=02:radio::1
+email_for_send=03:string:::
+use_input_name=04:radio::1
+use_input_email=05:radio::1
+use_input_phone=06:radio::1
 input_phone_countnum=07:range:1,20:12
-use_input_subj=08:radio:0,1:1
-input_necessarily=09:radio:0,1:1
+use_input_subj=08:radio::1
+input_necessarily=09:radio::1
 email_subj_add=10:string::EasyContact
 [END_COT_EXT_CONFIG]
- 
 ==================== */
